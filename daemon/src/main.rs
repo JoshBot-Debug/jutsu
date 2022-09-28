404b243e4e98cc5cd6bc4b88459a8a154ccc43d7
JoshBot-Debug <joshuamyerswebdev@gmail.com>
2022-09-28 21:28:43 +0530

    updated

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,4 +1,5 @@
-use std::net;
+use std::{net, sync::{Arc, Mutex}};
+use std::io::Write;
 
 use jutsu_cli::{Cli, CommandType};
 use jutsu_core::{Datagram, segment::{self, byte}};
@@ -17,9 +18,11 @@ fn main() -> std::io::Result<()> {
 
         let mut datagram = Datagram::new();
 
-        let mut timeout: usize = 5;
+        let timeout = Arc::new(Mutex::new(5));
 
-        let mut arg_ip_address: Option<String> = None;
+        let c_timeout = Arc::clone(&timeout);
+
+        let arg_ip_address: Arc<Mutex<Option<String>>> = Arc::new(Mutex::new(None));
 
         cli.commands()
             .iter()
@@ -31,14 +34,16 @@ fn main() -> std::io::Result<()> {
                     }
                 }
                 CommandType::IpAddress => {
-                    arg_ip_address = command.value().clone();
+                    let mut ip_address = arg_ip_address.lock().unwrap();
+                    *ip_address = command.value().clone();
                     println!("IpAddress {:?}",  command.value());
                 }
                 CommandType::Timeout => {
                     if let Some(time) = command.value() {
                         if let Ok(t) = time.parse::<usize>()
                         {
-                            timeout = t;
+                            let mut ct = c_timeout.lock().unwrap();
+                            *ct = t;
                             println!("Timeout {:?}", timeout);
                         }
                     }
@@ -61,41 +66,71 @@ fn main() -> std::io::Result<()> {
             }
         });
         
-        let bytes_received = std::sync::Arc::new(std::sync::Mutex::new(0));
-        let packets_received = std::sync::Arc::new(std::sync::Mutex::new(0));
+        let packets_received = Arc::new(Mutex::new(0));
+        let bytes_received = Arc::new(Mutex::new(0));
 
-        let pr = std::sync::Arc::clone(&packets_received);
-        let br = std::sync::Arc::clone(&bytes_received);
+        let packets_received_clone1 = Arc::clone(&bytes_received);
+        let bytes_received_clone1 = Arc::clone(&packets_received);
+
+        let packets_received_clone2 = Arc::clone(&bytes_received);
+        let bytes_received_clone2 = Arc::clone(&packets_received);
+
+        let packets_received_clone3 = Arc::clone(&bytes_received);
+        let bytes_received_clone3 = Arc::clone(&packets_received);
+
+
+        let targets = Arc::new(Mutex::new(cli.targets().len()));
+
+        let targets_1 =  Arc::clone(&targets);
+        let targets_2 =  Arc::clone(&targets);
+
+        let ip_address_1 =  Arc::clone(&arg_ip_address);
+        let ip_address_2 =  Arc::clone(&arg_ip_address);
 
         ctrlc::set_handler(move || {
-            print!("\n");
-            let pr = pr.lock().unwrap();
-            let br = br.lock().unwrap();
-            if let Some(ip_arg) = &arg_ip_address
-            {
-                println!("--- {} jutsu statistics ---", ip_arg);
-            }
-            println!("{} packets transmitted, {:?} packets ({:?} bytes) received ", &cli.targets().len(), &pr, &br);
-            std::process::exit(0);
+
+            let pr = packets_received_clone1.lock().unwrap();
+            let br = bytes_received_clone1.lock().unwrap();
+              
+            statistics(*pr, *br, &ip_address_1.lock().unwrap(), &targets_1.lock().unwrap())
         })
         .expect("Error setting Ctrl-C handler");
 
+        let mut stdout = std::io::stdout();
+
+        std::thread::spawn(move || {
+            loop {
+                std::thread::sleep(std::time::Duration::from_secs(1));
+
+                let mut ct = c_timeout.lock().unwrap();
+                *ct -= 1;
+                print!(".");
+                stdout.flush().unwrap();
+                
+                if *ct < 1
+                {
+                    let pr = packets_received_clone2.lock().unwrap();
+                    let br = bytes_received_clone2.lock().unwrap();
+                    statistics(*pr, *br, &ip_address_2.lock().unwrap(), &targets_2.lock().unwrap())
+                }
+            }
+        });
+
         loop {
             let mut buf = vec![0; byte::RESPONSE_SIZE];
-            let packets_received = std::sync::Arc::clone(&packets_received);
-            let bytes_received = std::sync::Arc::clone(&bytes_received);
 
             match socket.recv_from(&mut buf) {
                 Ok((size, from)) =>
                 {
-                    let mut pr = packets_received.lock().unwrap();
-                    let mut br = bytes_received.lock().unwrap();
+                    let mut pr = packets_received_clone3.lock().unwrap();
+                    let mut br = bytes_received_clone3.lock().unwrap();
                     *pr += 1;
                     *br += size;
 
                     let r = segment::output_from_buf(&mut buf);
                     println!("\nReceived reply ({} bytes) from {}", size, from.ip());
                     println!("{}", r);
+                    println!("");
                 }
                 Err(_) => error("Failed to receive packet.")
             }
@@ -103,6 +138,16 @@ fn main() -> std::io::Result<()> {
     }
 
     Ok(())
+}
+
+fn statistics(packets_received: usize, bytes_received: usize, arg_ip_address: &Option<String>, targets: &usize)
+{
+    if let Some(ip_arg) = &arg_ip_address
+    {
+        println!("\r--- {} jutsu statistics ---", ip_arg);
+    }
+    println!("{} packets transmitted, {:?} packets ({:?} bytes) received ", &targets, &packets_received, &bytes_received);
+    std::process::exit(0);
 }
 
 fn error(message: &str) -> ! {
