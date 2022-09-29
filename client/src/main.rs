a8de6a44c558bb813d1f61f4742417aa94c0b6e3
JoshBot-Debug <joshuamyerswebdev@gmail.com>
2022-09-29 22:50:44 +0530

    rework started

--- client/src/main.rs
+++ client/src/main.rs
@@ -17,8 +17,13 @@ fn main() {
         Ok((packet, from)) =>
         {
             dbg!(&packet);
-            dbg!(&packet);
             
+            // /**
+            //  * Response::from_cli_buf() // Returns an object containg the result as buf from a cli buf
+            //  * 
+            //  * on the server
+            //  * Response::from_buf() // converts a result buf to Response.
+            //  */
             dbg!(cli::Args::from_buf(&packet.to_vec()).unwrap());
             println!("[CLIENT] Received packet from {}", from);
             socket.send(&packet,&from);
